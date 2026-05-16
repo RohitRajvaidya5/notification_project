57968942fb87f695cbac5e95cb42a86c33be228e
rohitrajvaidya5 <rohit.rajvaidyaa@gmail.com>
2026-05-16 12:14:21 +0530

    16 May 2026 : Time Changes Revert Back

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+import time
 from notifier import send_notification
 from datetime import datetime
 
-current_hour = datetime.now().hour
 
-# Send notifications only between 7 AM and 11 PM
-if 7 <= current_hour < 23:
-    send_notification("Drink Water 💧", "Hydration check!")
-    print("Notification sent!")
-else:
-    print("Quiet hours. No notification sent.")
+
+# Drink water remainder every hour
+if __name__ == "__main__":
+    while True:
+
+        current_hour = datetime.now().hour
+
+
+        if 0 <= current_hour < 7:
+            # Sleep until 7 AM
+            hours_until_7am = (7 - current_hour) % 24
+            time.sleep(hours_until_7am * 3600)
+            print("It's 7 AM! Time to start your hydration routine.")
+        
+        else:
+
+            send_notification("Drink Water 💧", "Hydration check!")
+            time.sleep(3600)
