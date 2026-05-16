f1c1b1551c5dfff5e14181c2036bd3dfb71335be
rohitrajvaidya5 <rohit.rajvaidyaa@gmail.com>
2026-05-16 12:23:49 +0530

    16 May 2026 : Time Changes Revert Back

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ from datetime import datetime
 if __name__ == "__main__":
     while True:
 
-        current_hour = datetime.now().hour
+        # current_hour = datetime.now().hour
 
 
-        if 0 <= current_hour < 7:
-            # Sleep until 7 AM
-            hours_until_7am = (7 - current_hour) % 24
-            time.sleep(hours_until_7am * 3600)
-            print("It's 7 AM! Time to start your hydration routine.")
+        # if 0 <= current_hour < 7:
+        #     # Sleep until 7 AM
+        #     hours_until_7am = (7 - current_hour) % 24
+        #     time.sleep(hours_until_7am * 3600)
+        #     print("It's 7 AM! Time to start your hydration routine.")
         
-        else:
+        # else:
 
-            send_notification("Drink Water 💧", "Hydration check!")
-            time.sleep(3600)
+        #     send_notification("Drink Water 💧", "Hydration check!")
+        #     time.sleep(3600)
+        send_notification("Drink Water 💧", "Hydration check!")
+
