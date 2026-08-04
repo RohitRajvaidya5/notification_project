45c03eff9f43da48458537b3b50ed66e286c24de
rohitrajvaidya5 <rajvaidyarohit25@outlook.com>
2026-08-04 23:39:37 +0530

    Sending notification every hour

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from notifier import send_notification_with_ntfy, DEFAULT_TOPIC
+from datetime import datetime
+import time
+
+
+def send_every_hour_notification():
+    while True:
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        send_notification_with_ntfy(
+            topic="hourly-tracker",
+            title="Hourly Notification",
+            message=f"Current time: {current_time} : Let's keep track of the time and stay productive!",
+            tags=["hourly"],
+            priority=4
+        )
+        time.sleep(3600)
 
 
 if __name__ == "__main__":
-
-    send_notification_with_ntfy(
-    topic=DEFAULT_TOPIC,
-    title="Test Notification Alert",
-    message="This is the test notification just for the demo.",
-    tags=["computer"],
-    priority=5
-)
+    send_every_hour_notification()
